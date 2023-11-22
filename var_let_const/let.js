8f50f4f0e37709a2f6893b1183b17f345519064a
Muhammad Hafeezullah <146739943+PyGularCoder@users.noreply.github.com>
2023-11-22 07:33:48 +0000

    let in javascript

---
+++ var_let_const/let.js
@@ -0,0 +1,12 @@
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+  
+  let message = "Welcome to my website!";
+  if (true) {
+    let name = "John Doe";
+    greet(name);
+  }
+  
+  console.log(message);
+  
