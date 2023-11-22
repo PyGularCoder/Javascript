0161d91928512d70c8e61e479dfe4afc175d0198
Muhammad Hafeezullah <146739943+PyGularCoder@users.noreply.github.com>
2023-11-22 07:39:22 +0000

    const in javascript

---
+++ var_let_const/const.js
@@ -0,0 +1,7 @@
+const PI = 3.14159; // Constant value for pi
+const gravity = 9.81; // Constant value for Earth's gravity
+
+// PI = 3.15; // Attempt to reassign PI will result in an error
+
+console.log("Value of PI:", PI); // Output: Value of PI: 3.14159
+console.log("Value of gravity:", gravity); // Output: Value of gravity: 9.81
