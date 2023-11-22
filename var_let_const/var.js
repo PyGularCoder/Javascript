bbc8c267e54cd21c8eabda5f7802948af2c48536
Muhammad Hafeezullah <146739943+PyGularCoder@users.noreply.github.com>
2023-11-22 07:29:01 +0000

    var in javascript

---
+++ var_let_const/var.js
@@ -0,0 +1,14 @@
+//explanation in var.md file
+
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+  
+  var message = "Welcome to my website!";
+  if (true) {
+    var name = "John Doe";
+    greet(name);
+  }
+  
+  console.log(message);
+  
